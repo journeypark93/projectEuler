d045f3ea3ea5c8cd4797047c628d31b578757583
journeypark93 <wwwsomty@gmail.com>
2022-08-22 16:18:04 +0900

    2번 소인수 분해

---
+++ src/main/java/com/example/projecteuler/Problem3.java
@@ -0,0 +1,27 @@
+package com.example.projecteuler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem3 {
+    public static void main(String[] args){
+
+        System.out.println(Problem3.calPrime(600851475143L));
+    }
+
+    public static double calPrime(long num){
+
+        long k = 2L;
+        List<Long> primes = new ArrayList<>();
+        double sqrtNum = Math.sqrt(num);
+        while(k<=sqrtNum && k != 1){
+            if (num % k == 0){
+                num /= k;
+                primes.add(k);
+            } else {
+                k++;
+            }
+        }
+        return primes.get(primes.size()-1);
+    }
+}
