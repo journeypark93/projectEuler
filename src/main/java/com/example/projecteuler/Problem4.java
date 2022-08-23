6c0b89c1181ac3ed9f5fb0fb01a9b3ccf9d0d26e
journeypark93 <wwwsomty@gmail.com>
2022-08-23 17:00:40 +0900

    4번 대칭수찾기(palindrome)

---
+++ src/main/java/com/example/projecteuler/Problem4.java
@@ -0,0 +1,32 @@
+package com.example.projecteuler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Problem4 {
+    public static void main(String[] args){
+
+    List<Integer> pals = new ArrayList<>();
+
+        for (int x=999; x>=888; x--){
+            for (int i=999; i>=888; i--){
+                int num = i*x;
+                int numLength = String.valueOf(num).length();
+                String num2 = "";
+                for (int j = numLength-1; j >= 0; j--) {
+                    num2 += (num % 10);
+                    num /= 10;
+                }
+
+                if(num2.equals(String.valueOf(i*x))){
+                    pals.add(i*x);
+                    break;
+                }
+            }
+        }
+        Collections.sort(pals);
+        System.out.println(pals.get(pals.size()-1));
+    }
+}
